769711ffd9e24c8b9e95b07b9c7894ce1d1b5d90
yennifermazo27-ship-it <yennifermazo27@gmail.com>
2026-03-18 06:37:05 -0500

    Update ui.js

--- Scripts/ui.js
+++ Scripts/ui.js
@@ -1,22 +1,25 @@
-export function showWeather(city,weather){
+export function showWeather(city, weather) {
 
-const result = document.getElementById("result");
+    const result = document.getElementById("result");
 
-result.innerHTML = `
-<div class="card">
-<h2>${city}</h2>
-<p>Temperatura: ${weather.temperature} °C</p>
-<p>Viento: ${weather.windspeed} km/h</p>
-<p>Código clima: ${weather.weathercode}</p>
-<p>Hora: ${weather.time}</p>
-</div>
-`;
-
+    result.innerHTML = `
+    <div class="card">
+        <h2>${city}</h2>
+        <p> Temperatura: ${weather.temperature} °C</p>
+        <p> Viento: ${weather.windspeed} km/h</p>
+        <p> Código clima: ${weather.weathercode}</p>
+        <p> Hora: ${weather.time}</p>
+    </div>
+    `;
 }
 
-export function showError(message){
+export function showError(message) {
 
-const result = document.getElementById("result");
+    const result = document.getElementById("result");
 
-result.innerHTML = `<p>${message}</p>`;
+    result.innerHTML = `
+    <div class="card" style="background:#ffe5e5;">
+        <p style="color:red;">${message}</p>
+    </div>
+    `;
 }
